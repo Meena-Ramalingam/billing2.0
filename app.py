dd754c13a531c8762b4cabdc92e65a26740e9ac0
Meena R <211501049@rajalakshmi.edu.in>
2023-08-14 19:16:07 +0530

    .

--- app.py
+++ app.py
@@ -40,7 +40,10 @@ def send_email(subject, message, recipient):
 
 @app.route('/submit_bill', methods=['POST'])
 def submit_bill():
-    data = request.json.get('product_data')  # Retrieve JSON data from the request
+    print(request.headers)
+    data = request.json.get('product_data') 
+     # Retrieve JSON data from the request
+    print(data)
     
     # Process and update the database based on the JSON data
     conn = sqlite3.connect('product_database.db')
@@ -48,14 +51,18 @@ def submit_bill():
 
     for product in data:
         product_code = product['product_code']
+        print(product_code)
         quantity = product['quantity']
+        print(quantity)
 
         # Fetch the current present_stock for the product
         cursor.execute('SELECT minimum_stock,present_stock FROM products WHERE product_code = ?', (product_code,))
         row = cursor.fetchone()
+        print(row)
 
         if row is not None:
-            current_stock, minimum_stock = row
+            current_stock = row[1]
+            minimum_stock= row[0]
             new_stock = current_stock - int(quantity)
             cursor.execute('UPDATE products SET present_stock = ? WHERE product_code = ?', (new_stock, product_code))
         if new_stock <= minimum_stock:
@@ -69,7 +76,7 @@ def submit_bill():
     conn.close()
 
     # Redirect to the "Thank You" page
-    return redirect(url_for('thank_you'))
+    return "success"
 
 @app.route('/thank_you')
 def thank_you():
@@ -82,7 +89,9 @@ def index():
 @app.route('/get_product_data', methods=['POST'])
 def get_product_data():
     product_code = request.json.get('product_code')
+    
     product_data = fetch_product_data(product_code)
+    print(product_data)
     return jsonify({'product_name': product_data[0], 'price': product_data[1]})
 
 if __name__ == '__main__':
